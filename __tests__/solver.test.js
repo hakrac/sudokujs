92d0173a632f06677fde5f9b1478d892c2651c95
hakrac <hakimrachidi02@gmail.com>
2020-08-30 18:01:22 +0200

    Add backtracing solver

--- __tests__/solver.test.js
+++ __tests__/solver.test.js
@@ -1,6 +1,9 @@
 import { 
     Solver
-} from '../lib/solver'
+} from '../lib/backtracing'
+import {
+    formatter
+} from '../lib/formatter'
 
 
 describe('solver', () => {
@@ -32,8 +35,11 @@ describe('solver', () => {
             [9, 6, 8, 3, 5, 1, 7, 2, 4],
             [4, 1, 2, 6, 7, 9, 3, 5, 8],
         ])
+    })
 
-        // sudoku = [
+    test('should solve hard sudoku', () => {
+
+        // let sudoku = [
         //     [0, 0, 2, 0, 0, 0, 1, 0, 0],
         //     [7, 0, 0, 4, 0, 3, 0, 0, 6],
         //     [0, 0, 8, 2, 0, 9, 7, 0, 0],
@@ -45,9 +51,62 @@ describe('solver', () => {
         //     [0, 0, 7, 0, 0, 0, 5, 0, 0]
         // ]
 
-        // sudoku = Solver(sudoku)
+        let sudoku = [
+            [0, 0, 0, 0, 9, 0, 7, 4, 1],
+            [7, 0, 0, 4, 5, 2, 0, 0, 0],
+            [9, 0, 4, 0, 7, 0, 0, 8, 0],
+            [0, 0, 0, 0, 4, 0, 0, 7, 0],
+            [4, 0, 7, 0, 8, 6, 0, 1, 0],
+            [2, 0, 0, 0, 0, 7, 0, 0, 0],
+            [0, 7, 0, 0, 0, 0, 0, 0, 3],
+            [0, 0, 0, 7, 6, 0, 0, 2, 0],
+            [0, 6, 0, 3, 0, 5, 0, 0, 7]
+        ]
 
-        // console.log(sudoku)
+        sudoku = Solver(sudoku)
+
+        console.log(sudoku)
+    })
+
+    test('possible', () => {
+        let sudoku = [
+            [0, 0, 2, 0, 0, 0, 1, 0, 0],
+            [7, 0, 0, 4, 0, 3, 0, 0, 6],
+            [0, 0, 8, 2, 0, 9, 7, 0, 0],
+            [5, 0, 0, 0, 9, 0, 0, 0, 4],
+            [0, 0, 3, 0, 0, 0, 6, 0, 0],
+            [1, 0, 0, 0, 3, 0, 0, 0, 2],
+            [0, 0, 1, 3, 0, 6, 9, 0, 0],
+            [8, 0, 0, 5, 0, 7, 0, 0, 3],
+            [0, 0, 7, 0, 0, 0, 5, 0, 0]
+        ]
+
+        let ispossible = possible(sudoku, 0, 0, 1)
+        expect(ispossible).toBeFalsy()
+        ispossible = possible(sudoku, 0, 0, 8)
+        expect(ispossible).toBeFalsy()
+        ispossible = possible(sudoku, 0, 0, 9)
+        expect(ispossible).toBeTruthy()
 
     })
+
+    test.only('should solve via backtracing', () => {
+        let sudoku = [
+            [0, 0, 5, 0, 9, 0, 2, 0, 0],
+            [8, 0, 0, 0, 3, 0, 0, 0, 5],
+            [0, 0, 3, 5, 0, 6, 1, 0, 0],
+            [0, 2, 9, 0, 1, 0, 6, 4, 0],
+            [0, 0, 0, 7, 0, 3, 0, 0, 0],
+            [0, 5, 6, 0, 4, 0, 8, 3, 0],
+            [0, 0, 7, 4, 0, 8, 9, 0, 0],
+            [9, 0, 0, 0, 5, 0, 0, 0, 4],
+            [0, 0, 2, 0, 7, 0, 3, 0, 0],
+        ]
+
+        let results = Solver(sudoku)
+        for(let result of results) {
+            formatter(result)
+        }
+    })
+
 })
